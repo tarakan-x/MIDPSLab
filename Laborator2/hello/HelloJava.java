432de908552037822b12c5b806e25630cf745cf4
Chifa Vladislav <vlad.chifa26@gmail.com>
2025-09-14 18:43:58 +0300

    Delete Laborator2 directory

--- Laborator2/hello/HelloJava.java
+++
@@ -1,7 +0,0 @@
-public class HelloJava {
-
-    public static void main(String[] args) {
-        System.out.println("\nHello,welcome to GitHub!");
-    }
-
-}
--- Laborator2/hello/hello.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print "\nWelcome to GITHUB!\n"
-
-if __name__=="__main__":
-    main()
